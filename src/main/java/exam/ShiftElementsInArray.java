c4573dfb694a57c6a0f919523c48a8368655d1cf
Nisha2210 <86303934+Nisha2210@users.noreply.github.com>
2021-06-23 00:10:26 +0530

    Update ShiftElementsInArray.java

--- src/main/java/exam/ShiftElementsInArray.java
+++ src/main/java/exam/ShiftElementsInArray.java
@@ -2,9 +2,26 @@ package exam;
 
 public class ShiftElementsInArray {
 
-  public void shift(int[] arr) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-
+  public static void main(String[] args) {
+     int [] arr= new int[]{1,2,3,4,5};
+    int n=3;
+    System.out.println("Array without shifting");
+    for(int i=0;i<arr.length;i++){
+      System.out.print(arr[i]+"");
   }
-
+    for(int i=0;i<n;i++)
+      int j,end;
+      end=arr[arr.length-1];
+    for(j=arr.length-1;j>0;j--){
+      arr[j]=arr[j-1];
+    }
+    arr[0]=end;
+   }
+  System.out.println();
+  System.out.println("After Shifting ");
+  for(int i=0;i<arr.length;i++)
+  {
+    System.out.print(arr[i]+"");
+  }
+}
 }
