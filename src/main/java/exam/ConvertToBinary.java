64ce6c79b151f6600f2923f3d17014c84c3b5a04
Nisha2210 <86303934+Nisha2210@users.noreply.github.com>
2021-06-22 23:50:13 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -3,7 +3,19 @@ package exam;
 public class ConvertToBinary {
 
   public String toBinary(final int n) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+    int bi[]=new int[40];
+    int index=0;
+    while(n>0){
+      bi[index++]=n%2;
+      n=n/2;
+    }
+    for(int i=index-1;i>=0;i--){
+      System.out.print(bi[i]);
+    }
+    System.out.println();
+  }
+  public static void main(String args[]){
+    System.out.println("demical of 10");
+    toBinary(10);
   }
 }
